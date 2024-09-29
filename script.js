4957f4b5609dc53b11df35d7edc3539970c9b2ba
selvi s <selvi.subbiah96@gmail.com>
2024-09-29 19:03:47 +0400

    javascriptbasics

---
+++ script.js
@@ -0,0 +1,10 @@
+function joinedName(){
+
+    var firstname = document.getElementById("frstname");
+    var lastname = document.getElementById("surname");
+    var submit = document.getElementById("result");
+
+   submit.innerHTML += firstname.value + lastname.value;
+
+   submit.style.color ="#6e370b";
+}
